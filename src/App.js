894b36e4cf38b51eb606ccdbaeb4456135235636
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-01-22 10:53:18 +0900

    Add fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+
 function App() {
-  return <div>Setup</div>;
+  return (
+    <>
+      <Headline>Marie-Carleen Koizumi</Headline>
+      <SubHeadline>Portfolio & Personal Website</SubHeadline>
+      <p>A work in progress...</p>
+    </>
+  );
 }
 
 export default App;
+
+const Headline = styled.h1`
+  font-family: var(--font-primary);
+  font-weight: normal;
+`;
+
+const SubHeadline = styled.h2`
+  font-weight: 300;
+`;
--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,6 +1,53 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+
+/* -------------------- FONTS START -------------------- */
+
+/* leckerli-one-regular - latin */
+@font-face {
+  font-family: 'Leckerli One';
+  font-style: normal;
+  font-weight: 400;
+  src: url('/fonts/leckerli-one-v16-latin-regular.eot'); /* IE9 Compat Modes */
+  src: local(''),
+       url('/fonts/leckerli-one-v16-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('/fonts/leckerli-one-v16-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+       url('/fonts/leckerli-one-v16-latin-regular.woff') format('woff'), /* Modern Browsers */
+       url('/fonts/leckerli-one-v16-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+       url('/fonts/leckerli-one-v16-latin-regular.svg#LeckerliOne') format('svg'); /* Legacy iOS */
+}
+
+/* roboto-300 - latin */
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 300;
+  src: url('/fonts/roboto-v30-latin-300.eot'); /* IE9 Compat Modes */
+  src: local(''),
+       url('/fonts/roboto-v30-latin-300.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('/fonts/roboto-v30-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
+       url('/fonts/roboto-v30-latin-300.woff') format('woff'), /* Modern Browsers */
+       url('/fonts/roboto-v30-latin-300.ttf') format('truetype'), /* Safari, Android, iOS */
+       url('/fonts/roboto-v30-latin-300.svg#Roboto') format('svg'); /* Legacy iOS */
+}
+
+/* roboto-regular - latin */
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  src: url('/fonts/roboto-v30-latin-regular.eot'); /* IE9 Compat Modes */
+  src: local(''),
+       url('/fonts/roboto-v30-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('/fonts/roboto-v30-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+       url('/fonts/roboto-v30-latin-regular.woff') format('woff'), /* Modern Browsers */
+       url('/fonts/roboto-v30-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+       url('/fonts/roboto-v30-latin-regular.svg#Roboto') format('svg'); /* Legacy iOS */
+}
+
+/* -------------------- FONTS END -------------------- */
+
 *,
 *:before,
 *:after {
@@ -12,11 +59,15 @@ export default createGlobalStyle`
     --secondary-silver: #7093bc;
     --light-primary: #f0faff;
     --light-green: #e6f4f1;
+
+    --font-primary: 'Leckerli One', cursive;
+    --font-secondary: 'Roboto', sans-serif;
 }
 
 body {
     margin: 0;
-    font-family: sans-serif;
+    font-family: var(--font-secondary);
+    font-weight: 300;
     font-size: 112.5%;
     line-height: 1.5;
 }
