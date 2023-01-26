94deb4e4424c04119a04f46e9c4926bcdfec2390
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-01-26 19:36:29 +0900

    Change text and typos

--- src/components/About.js
+++ src/components/About.js
@@ -7,32 +7,33 @@ export default function About() {
       <h2>About me</h2>
       <p>
         Hi! Welcome to my portfolio / website. I created this space to introduce
-        myself and summarize some previous projects I have worked on. My web
+        myself and summarize some of the projects I have worked on. My web
         development journey started four years ago when I signed up for an
         intensive “R” course at university. It lasted only a week, but I felt
         excited and wanted to know more about programming. I started to play
-        around using HTML and CSS on my own. Seemingly creating something new
-        out of nowhere captured my curiosity and I decided to visit a Web
-        Development Bootcamp in Germany. For three months I would learn and
-        write code every day and immerse myself in this unfamiliar and exciting
-        new world.
+        around with HTML and CSS on my own. Seemingly creating something new out
+        of nowhere captured my curiosity and I decided to attend a Web
+        Development Bootcamp in Germany. For three months I was learning and
+        writing code every day and immersed myself in this unfamiliar and
+        exciting new world.
       </p>
       <p>
-        After the bootcamp finished, I started as a junior coach and web
-        developer at the same bootcamp. First as a working student and after I
-        decided to ditch my studies, as a full-time employee. For a bit over two
-        years I have worked with and taught modern web technologies such as CSS,
-        HTML, JavaScript (ES6), React and NodeJS to over 100 students. Through
-        both working as a developer and a coach I have a good understanding of
-        best practices, clean code and modern JavaScript in general.
+        After the bootcamp finished, I started as a Junior Coach and Web
+        Developer at the same bootcamp. First as a working student and after I
+        decided to pursue a career in coding, as a full-time employee. For the
+        last two years I have worked with and taught modern web technologies
+        such as CSS, HTML, JavaScript (ES6), React and NodeJS to over 100
+        students. Through both working as a developer and a coach I have a good
+        understanding of best practices, clean code and modern JavaScript in
+        general.
       </p>
       <p>
-        A personal choice to move across the globe to Japan made me leave my
-        job. Since then I have shifted my focus to learning Japanese and working
-        on some side projects. Currently, I am looking for a new challenge as a
-        frontend developer.
+        A personal choice to move across the globe to Japan required me to leave
+        my job and since then I have shifted my focus to learning Japanese and
+        building up my portfolio. Currently, I am looking for a new challenge as
+        a Frontend Developer.
       </p>
-      <p>I would love to connect with you!</p>
+      <p>Please reach out - I would love to connect with you!</p>
       <LinkButton href="mailto:mariecarleen.koizumi@gmail.com">
         Send me an email!
       </LinkButton>
