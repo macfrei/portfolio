e56377240ff5cc4be8f13db398dc46bdf099d2a6
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-01-26 15:43:22 +0900

    Implement new components

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import FocusLock from "react-focus-lock";
 
+import SocialMenu from "./SocialMenu";
+
 export default function BurgerMenu({ links }) {
   const ref = useRef();
   const [isOpen, setIsOpen] = useState(false);
@@ -46,6 +48,7 @@ export default function BurgerMenu({ links }) {
                   {name}
                 </Link>
               ))}
+              <SocialMenu color="white" />
             </BurgerMenuStyled>
           </Section>
         </FocusLock>
@@ -55,7 +58,7 @@ export default function BurgerMenu({ links }) {
 }
 
 const Section = styled.section`
-  @media (min-width: 767px) {
+  @media (min-width: 768px) {
     display: none;
   }
 `;
